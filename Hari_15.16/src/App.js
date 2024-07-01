a6f610075f4ea1430ec93b89b90d3dc72192cd78
Motan17 <140112643+Motan17@users.noreply.github.com>
2024-07-01 18:28:15 +0700

    Add files via upload

---
+++ Hari_15.16/src/App.js
@@ -0,0 +1,20 @@
+import logo from './logo.svg';
+import './App.css';
+import Header from './header';
+import Content from './Contentt';
+import Isi from './jam';
+import Sidebar from './sidebar';
+
+function App() {
+  return (
+    <div className="container">
+      
+<Content/>
+<Isi/>
+<Sidebar/>
+
+
+      </div>
+  )}
+
+export default App;
